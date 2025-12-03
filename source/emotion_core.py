073580139e0e75802bd1d85b8aec9e0a7d3877f1
qinghai-ovo <qinghai1027@gmail.com>
2025-12-03 18:14:52 +0900

    Update TODO comment

--- source/emotion_core.py
+++ source/emotion_core.py
@@ -3,11 +3,24 @@
 import time
 import random
 
+#Loading model and Opening camera
+#Return: True / False
 def init_system():
     print("[analog] Loading model")
     print("[analog] Opening the camera")
     return True
 
+# reading fram and analize
+# Return: Dictionary
+#     {
+#         "hasFace": True,     
+#         "valence": 0.5,       # (-1 ~ 1)
+#         "arousal": 0.1,       #  (-1 ~ 1)
+#         "intensity": 0.8,     #  (0 ~ 1)
+#         "label": "Happy"      
+#     }
+# if face are not detected 
+# Return: {"hasFace": False}
 def get_current_emotion():
     # fake data for test
     return {
@@ -18,5 +31,6 @@ def get_current_emotion():
         "label": random.choice(["Happy", "Sad", "Neutral", "Angry"])
     }
 
+# Release camera and other resource
 def release_system():
     print("[analog] realse resource")
